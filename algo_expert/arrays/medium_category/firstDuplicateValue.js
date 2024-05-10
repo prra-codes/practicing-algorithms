90fe0f160a4f302553b2b919059bba4e61b140db
peter <peter.arth5@gmail.com>
2024-05-10 12:47:40 +0100

    done 1 Algo Expert qn first duplicate, arrays section medium category. Done 1 7 kyu Codewars qn small enough beginner

---
+++ algo_expert/arrays/medium_category/firstDuplicateValue.js
@@ -0,0 +1,48 @@
+// Day 5, Fri, 9/05/24 (Day 4 rest day for algo expert)
+
+// 14. First Duplicate Value
+
+// Given an array of integers between 1 and n, inclusive, where n is the length of the array, write a function that returns the first integer that appears more than once (when the array is read from left to right).
+
+// In other words, out of all the integers that might occur more than once in the input array, your function should return the one whose first duplicate value has the minimum index.
+
+// If no integer appears more than once, your function should return -1.
+
+// Note that you're allowed to mutate the input array.
+
+// Sample Input #1
+
+// array = [2, 1, 5, 2, 3, 3, 4]
+
+// Sample Output #1
+
+// 2 // 2 is the first integer that appears more than once.
+// 3 also appears more than once, but the second 3 appears after the second 2.
+
+// Sample Input #2
+
+// array = [2, 1, 5, 3, 3, 2, 4]
+
+// Sample Output #2
+
+// 3 // 3 is the first integer that appears more than once.
+// 2 also appears more than once, but the second 2 appears after the second 3.
+
+function firstDuplicateValue(array) {
+  const seen = new Set();
+
+  for (const item of array) {
+    if (seen.has(item)) {
+      return item;
+    }
+
+    seen.add(item);
+  }
+
+  return -1;
+}
+
+// O(n) time, go through array
+// O(n) space, creating set
+
+console.log(firstDuplicateValue([2, 1, 5, 2, 3, 3, 4]));
---
+++ codewars/seven_kyu/small_enough_beginner.js
@@ -0,0 +1,25 @@
+// Day 5, Sat, 9/05/24
+
+// 22. Small enough? - Beginner
+
+// You will be given an array and a limit value. You must check that all values in the array are below or equal to the limit value. If they are, return true. Else, return false.
+
+// You can assume all values in the array are numbers.
+
+function smallEnough(a, limit) {
+  const upperLimit = [];
+
+  for (let i = 0; i < a.length; i++) {
+    if (a[i] > limit) {
+      upperLimit.push(a[i]);
+    }
+  }
+
+  if (upperLimit.length === 0) {
+    return true;
+  }
+
+  return false;
+}
+
+console.log(smallEnough([1, 2, 3, 4, 5], 3));
